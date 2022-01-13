8e116c4fbbf91744d6e624547db1c5a91634c65f
Shane Li <61711097+shane1595042264@users.noreply.github.com>
2022-01-13 12:29:21 -0500

    quick fix

---
+++ emotrail_frontend/src/components/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import GoogleLogin from 'react-google-login';
+import {useNavigate} from 'react-router-dom';
+import {FcGoogle} from 'react-icons/fc'; /* this is just for google logo */
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
+import logo2 from '../assets/emo.png';
+const Login = () => {
+    return (
+        <div>
+            Login
+        </div>
+    )
+}
+
+export default Login
