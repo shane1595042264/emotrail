ac8a63a07f5e0d0db50b6be3544194b2ed3c29ec
Shane Li <61711097+shane1595042264@users.noreply.github.com>
2022-01-13 17:43:29 -0500

    Update Login.jsx

--- emotrail_frontend/src/components/Login.jsx
+++ emotrail_frontend/src/components/Login.jsx
@@ -7,8 +7,42 @@ import logo from '../assets/logowhite.png';
 import logo2 from '../assets/emo.png';
 const Login = () => {
     return (
-        <div>
-            Login
+        <div className='flex justify-start items-center flex-col h-screen'> 
+        {/* flex col to place items vertically. 
+        Use h-screen to make an element span the entire height of the viewport. */}
+            <div className='relative w-full h-full'>
+                <video 
+                src={shareVideo}  
+                // you can change video here
+                type='video/mp4' 
+                controls={false} 
+                loop 
+                muted 
+                autoPlay 
+                className='w-full h-full object-cover'
+                />
+                <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+                    <div className='p-5'>
+                        <img src={logo2}  width= "330" alt="logo"/>
+                    </div>
+
+                    <div className='shadow-2xl'>
+                        <GoogleLogin 
+                            clientId=''
+                            render={(renderProps)=>(
+                                <button
+                                type='button'
+                                className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
+                                onClick={renderProps.onClick}
+                                >
+                                <FcGoogle className='mr-4'/> Sign in with Google
+
+                                </button>
+                            )}  
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
