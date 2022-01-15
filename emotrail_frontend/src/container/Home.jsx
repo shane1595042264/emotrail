fdaec028456919ffd3eff0732218dbcac9c9e957
Shane Li <61711097+shane1595042264@users.noreply.github.com>
2022-01-15 08:49:17 -0500

    Update Home.jsx

--- emotrail_frontend/src/container/Home.jsx
+++ emotrail_frontend/src/container/Home.jsx
@@ -10,15 +10,20 @@ import logo from '../assets/logo3.png';
 
 const Home = () => {
     const [toggleSidebar, setToggleSidebar] = useState(false)
-
     const [user, setUser] = useState(null)
+    const scrollRef = useRef(null);
 
     const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')):localStorage.clear();
+    
 
     useEffect(() => {
         const query = userQuery(userInfo?.googleId); // "?." is optional chianing
         client.fetch(query)
             .then((data) => {setUser(data[0]);})
+    }, [])
+
+    useEffect(() => {
+        scrollRef.current.scrollTo(0,0)
     }, [])
 
     return (
@@ -28,9 +33,10 @@ const Home = () => {
         Use flex-row to position flex items horizontally in the same direction as text
          */}
          <div className='hidden md:flex h-screen flex-initial'>
-            <Sidebar/>
+            <Sidebar user={user && user} closeToggle={setToggleSidebar}/>
          </div>
             <div className='flex md:hidden flex-row'>
+            <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
             <HiMenu fontSize={40} className='cursor-pointer' onClick={()=>setToggleSidebar(true)}/>
             <Link to="/">
                 <img src={logo} alt="logo" className="w-28"/>
@@ -40,16 +46,31 @@ const Home = () => {
             </Link>
 
             </div>
+
             {
-                toggleSidebar && (
+                toggleSidebar && ( // This is conditional rendering from React
                     <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in '>
+                    {/*
+                    Overflow-y-auto, so that people can vertically scroll up and down.
+                    
+                     */}
                     <div className='absolute w-full flex justify-end items-center p-2'>
                     <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={()=> setToggleSidebar(false)}/>
                     </div>
-                    <Sidebar />
+                    <Sidebar user={user && user}  closeToggle={setToggleSidebar}/> 
+                    {/* this is also conditional rendering */}
                     </div>
-                )
-            }
+                )}
+            </div>
+                <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+                        <Routes>
+                            <Route path="/user-profile/:userId" element={<UserProfile/>}/>
+                            <Route path="/*" element={<Pins user={user && user}/>}/> 
+                            {/* Return user only if user exists */}
+
+                         
+                        </Routes>
+                </div>
         </div>
     )
 }
