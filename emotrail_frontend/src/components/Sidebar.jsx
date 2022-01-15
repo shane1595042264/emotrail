e1020d27f4a440c331822b2df615f287932e4b7f
Shane Li <61711097+shane1595042264@users.noreply.github.com>
2022-01-15 15:23:19 -0500

    Update Sidebar.jsx

--- emotrail_frontend/src/components/Sidebar.jsx
+++ emotrail_frontend/src/components/Sidebar.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import {NavLink, Link} from 'react-router-dom';
+import {RiHomeFill} from 'react-icons/ri';
+import {IoIosArrowsForward} from 'react-icons/io';
+import logo from '../assets/logo3.png';
 
-const Sidebar = () => {
+
+
+const Sidebar = (user, closeToggle) => {
+
+    const handleCloseSidebar = () => {
+        if(closeToggle) closeToggle(false)
+    }
     return (
-        <div>
-            Sidebar
+        <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar">
+            <div className="flex flex-col">
+                <Link
+                   to="/"
+                   className="flex px-5 gap-2 my-6 pt-1 w-190 items-center"
+                   onClick={handleCloseSidebar}
+                    >
+                <img src={logo} alt="logo" className=" w-full"/>
+                
+
+                </Link>
+                <div className="flex flex-col gap-5">
+                <NavLink
+                to="/"
+                className={({isActive}) => isActive ? isActiveStyle : isNotActiveStyle}
+                >
+
+                </NavLink>
+
+                </div>
+            </div>
         </div>
     )
 }
