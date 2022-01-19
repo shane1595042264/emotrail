f07950d4334704d1efe0746a1fd0ec6c24e2c8d4
Shane Li <61711097+shane1595042264@users.noreply.github.com>
2022-01-19 16:55:32 -0500

    Fixed imag.user bug

--- emotrail_frontend/src/container/Home.jsx
+++ emotrail_frontend/src/container/Home.jsx
@@ -13,18 +13,19 @@ const Home = () => {
     const [user, setUser] = useState(null)
     const scrollRef = useRef(null);
 
-    const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')):localStorage.clear();
+    const userInfo = localStorage.getItem('user')  !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
     
-
     useEffect(() => {
-        const query = userQuery(userInfo?.googleId); // "?." is optional chaining
-        client.fetch(query)
-            .then((data) => {setUser(data[0]);})
-    }, [])
+        const query = userQuery(userInfo?.googleId);
+
+        client.fetch(query).then((data) => {
+            setUser(data[0]);
+        });
+    }, []);
 
     useEffect(() => {
         scrollRef.current.scrollTo(0,0)
-    }, [])
+    });
 
     return (
         <div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
@@ -35,7 +36,7 @@ const Home = () => {
          <div className='hidden md:flex h-screen flex-initial'>
             <Sidebar user={user && user} closeToggle={setToggleSidebar}/>
          </div>
-            <div className='flex md:hidden flex-row'>
+            <div className='flex md:hidden w-full flex-row'>
             <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
             <HiMenu fontSize={40} className='cursor-pointer' onClick={()=>setToggleSidebar(true)}/>
             <Link to="/">
@@ -57,10 +58,13 @@ const Home = () => {
                      */}
                     <div className='absolute w-full flex justify-end items-center p-2'>
                     <AiFillCloseCircle fontSize={30} className='cursor-pointer' onClick={()=> setToggleSidebar(false)}/>
+                    
                     </div>
                     <Sidebar user={user && user}  closeToggle={setToggleSidebar}/> 
                     {/* this is also conditional rendering */}
+                    
                     </div>
+                    
                 )}
             </div>
                 <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
@@ -68,9 +72,8 @@ const Home = () => {
                             <Route path="/user-profile/:userId" element={<UserProfile/>}/>
                             <Route path="/*" element={<Pins user={user && user}/>}/> 
                             {/* Return user only if user exists */}
-
-                         
                         </Routes>
+                        
                 </div>
         </div>
     )
